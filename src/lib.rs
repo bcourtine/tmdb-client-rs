884beb8f2fdd61519adb5c53c513ee92f1e74b98
Benoît Courtine <benoit.courtine@gmail.com>
2025-09-01 10:18:26 +0200

    ♻️ fixes #4. Better error management, using 'thiserror' crate.

--- src/lib.rs
+++ src/lib.rs
@@ -10,27 +10,14 @@ pub mod apis;
 pub mod models;
 pub mod files;
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    Reqwest(reqwest::Error),
-    Serde(serde_json::Error),
-    Io(std::io::Error),
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(e: reqwest::Error) -> Self {
-        Error::Reqwest(e)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(e: serde_json::Error) -> Self {
-        Error::Serde(e)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Error::Io(e)
-    }
+    #[error("HTTP request error")]
+    Reqwest(#[from] reqwest::Error),
+    
+    #[error("JSON serialization/deserialization error")]
+    Serde(#[from] serde_json::Error),
+    
+    #[error("I/O error")]
+    Io(#[from] std::io::Error),
 }
